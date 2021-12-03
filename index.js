c7dd99820e3bd51d296f3d3f4c789cfb34feefad
amy-chinlai <amy@actionnetwork.org>
2021-12-03 10:20:44 -0500

    commit tree 958a62197b961702873b7f2c74c8d54d68866848

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.querySelector('main#main')
+main.remove()
+
+document.body.innerHTML = "<h1 id='victory'>Amy is the champion</h1>"
+
+let newHeader = document.querySelector('h1#victory')
